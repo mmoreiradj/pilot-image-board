46ecf629b7eacd74ec74da0962e2aef6ccb3e8e7
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2022-12-03 21:03:56 +0100

    chore: initialize project

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCookie from '@fastify/cookie';
+import fastifyCsrf from '@fastify/csrf-protection';
+
+async function bootstrap() {
+  const CORS_OPTIONS = {
+    origin: ['http://localhost:8080', '*'],
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ],
+    credentials: true,
+    methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE', 'PATCH'],
+  };
+
+  const adapter = new FastifyAdapter();
+  adapter.enableCors(CORS_OPTIONS);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+  );
+
+  await app.register(fastifyCookie, {
+    secret: 'super-secret',
+  });
+
+  await app.register(fastifyCsrf);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
