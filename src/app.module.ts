baafbd69eed9dfe06ee1a0e511d4f64bf685808e
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2022-12-28 23:33:33 +0100

    feat: add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
